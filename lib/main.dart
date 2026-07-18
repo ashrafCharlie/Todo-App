1b6b74f445750423e47ceec575b07c8115970a7b
Ashraf sagor <ashrafsagor19@gmail.com>
2026-07-18 23:55:27 +0600

    firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class TodoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: null);
+    return MaterialApp(debugShowCheckedModeBanner: false,
+     home: Scaffold());
   }
 }
