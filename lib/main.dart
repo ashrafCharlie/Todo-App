0bb8cdcef40a40de2dfc1d3b3cef27b31b21638d
Ashraf sagor <ashrafsagor19@gmail.com>
2026-07-22 22:24:27 +0600

    added blocProvider for TodoBloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,59 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:todo_app/features/auth/data/firebase_auth_repo.dart';
+import 'package:todo_app/features/auth/domain/repo/auth_repo.dart';
+import 'package:todo_app/features/auth/presentation/cubit/auth_cubit.dart';
+import 'package:todo_app/features/auth/presentation/cubit/auth_state.dart';
 import 'package:todo_app/features/auth/presentation/screens/tooglePage.dart';
+import 'package:todo_app/features/todo/data/firebase_todo_repo.dart';
+import 'package:todo_app/features/todo/domain/repo/todo_repo.dart';
+import 'package:todo_app/features/todo/presentation/bloc/todo_bloc.dart';
+import 'package:todo_app/features/todo/presentation/bloc/todo_event.dart';
+import 'package:todo_app/features/todo/presentation/screens/todo_home_screen.dart';
 import 'package:todo_app/firebase_options.dart';
+import 'package:todo_app/shared/my_circle_progress_indicator.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const TodoApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(TodoApp());
 }
 
 class TodoApp extends StatelessWidget {
-  const TodoApp({super.key});
+  TodoApp({super.key});
+  final AuthRepo firebaseAuthRepo = FirebaseAuthRepo();
+  final TodoRepo todoRepo = FirebaseTodoRepo();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Tooglepage());
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthCubit(firebaseAuthRepo)..checkAuth(),
+        ),
+        BlocProvider(create: (context) => TodoBloc(todoRepo)..add(TodoLoadEvent())),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: BlocConsumer<AuthCubit, AuthState>(
+          listener: (context, state) {
+            if (state is AuthError) {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text(state.errorMsg)));
+            }
+          },
+          builder: (context, state) {
+            if (state is Authenticate) {
+              return TodoPage();
+            } else if (state is AuthLoading) {
+              return Center(child: MyCircleProgressIndicator());
+            } else {
+              return Tooglepage();
+            }
+          },
+        ),
+      ),
+    );
   }
 }
